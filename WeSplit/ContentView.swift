5c985a70e6b30a16cbd213456f94d3d8eca1b00d
stationAstral <cougarjonez@gmail.com>
2023-10-29 21:16:25 -0700

    1.3

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Text("Hello World")
+                Text("Hello World")
+                Text("Hello World")
+                Section {
+                    Text("yo")
+
+                }
+
+            }
+            .navigationBarTitle("SwiftUI")
+
         }
-        .padding()
     }
 }
 
