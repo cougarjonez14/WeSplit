75bcaded6dd8ec2efcd7556fb844279a29127ab6
stationAstral <cougarjonez@gmail.com>
2023-11-01 10:40:31 -0700

    1.3.1

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -5,24 +5,27 @@
 //  Created by stationAstral on 10/29/23.
 //
 
+//ENDED @ PART 5
+
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
-        NavigationStack {
-            Form {
-                Text("Hello World")
-                Text("Hello World")
-                Text("Hello World")
-                Section {
-                    Text("yo")
-
-                }
-
-            }
-            .navigationBarTitle("SwiftUI")
-
+        Button("Tap Count \(tapCount)") {        
+            tapCount += 1
+            
+            
         }
+        // make button a rectangle  with a border
+        .frame(width: 111, height: 25)
+        .border(Color.cyan)
+        .background(.blue)
+        .cornerRadius(10)
+        .foregroundStyle(.white)
+        
+        
     }
 }
 
